2d8341d81418926b94c0a265300017ff021d4ba7
Kiramuun <deimonlight@live.fr>
2024-06-17 17:41:19 +0200

    Update Disinteg2

--- Assets/Shaders/Disintegrations/Disintegration 2/DisintegController.cs
+++ Assets/Shaders/Disintegrations/Disintegration 2/DisintegController.cs
@@ -1,37 +1,45 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
-public class DissolvingController : MonoBehaviour
+public class DisintegController : MonoBehaviour
 {
-    public MeshRenderer SkinnedMesh;
-    public float dissolveRate = 0.0125f;
+    public SkinnedMeshRenderer skinnedMesh;
+    public GameObject Eyes;
+    public VisualEffect effect;
+    public float dissolveRate = 0.125f;
     public float refreshRate = 0.025f;
     private Material[] skinnedMaterials;
 
     private void Start()
     {
-        if(SkinnedMesh != null)
-            skinnedMaterials = SkinnedMesh.materials;
+        if(skinnedMesh != null)
+            skinnedMaterials = skinnedMesh.materials;
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            Eyes.SetActive(false);
             StartCoroutine(DissolveCo());
         }
     }
 
     IEnumerator DissolveCo()
     {
+        if(effect != null)
+        {
+            effect.Play();
+        }
+
         if(skinnedMaterials.Length > 0)
         {
             float counter = 0;
             while (skinnedMaterials[0].GetFloat("_DissolveAmount") < 1)
             {
                 counter += dissolveRate;
-                for (int i = 0; i < skinnedMaterials.Length; i++)
+                for(int i = 0; i < skinnedMaterials.Length; i++)
                 {
                     skinnedMaterials[i].SetFloat("_DissolveAmount", counter);
                 }
